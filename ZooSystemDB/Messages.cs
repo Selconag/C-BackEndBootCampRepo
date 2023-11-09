48b361167f15db712908fc21af2779365722e721
Selçuk Aksoy <Ooocrazyselcuk@gmail.com>
2023-11-09 11:05:09 +0300

    Update Messages.cs

--- ZooSystemDB/Messages.cs
+++ ZooSystemDB/Messages.cs
@@ -1,6 +1,6 @@
 public class Messages()
 {
-public static string AnimalNotFoundException(string animal)
+						public static string AnimalNotFoundException(string animal)
 						{
 											Console.WriteLine($"Aradığınız {animal} hayvan bulunamadı");
 						}
